e1283fd47fe425c2386de2ad5e14b5676c4e2a25
Niels Hoppe <mail@nielshoppe.de>
2024-04-15 13:56:48 +0200

    Add basic Python exercises

---
+++ src/basic_python_1.py
@@ -0,0 +1,60 @@
+import math
+
+def var_from_std(std: float) -> float:
+    '''
+    Calculate and return the variance from a given standard deviation.
+    '''
+    pass
+
+def std_from_var(var: float) -> float:
+    '''
+    Calculate and return the standard deviation from a given variance.
+    '''
+    pass
+
+def squared_dist(xi: float, x: float) -> float:
+    '''
+    Calculate and return the squared distance between xi and x.
+    '''
+    pass
+
+def sem(std: float, n: int) -> float:
+    '''
+    Return the standard error of the mean based on a given standard deviation and sample size.
+    '''
+    pass
+
+def mean(x1, x2, x3, x4, x5) -> float:
+    '''
+    Return the mean of five numbers.
+    This only serves as practice, not as a generalized solution!
+    '''
+    pass
+
+def median(x1, x2, x3, x4, x5) -> float:
+    '''
+    Return the median of five numbers.
+    This only serves as practice, not as a generalized solution!
+    '''
+    pass
+
+def season(month: int) -> str:
+    '''
+    Return the season matching the given month (1-12):
+
+    "Spring" for the months March, April, May
+    "Summer" for the months June, July, August
+    "Autumn" for the months September, October, November
+    "Winter" for the months December, January und February.
+    '''
+    pass
+
+def can_reject_h0(pval: float, alpha: float) -> bool:
+    '''
+    When pval is smaller than alpha return true,
+    otherwise return false.
+
+    Try solving once with a regular if-else statement
+    and once using the ternary operator (or "shorthand" if-else)
+    '''
+    pass
