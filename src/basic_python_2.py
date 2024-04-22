c02f98b233baa0066a9e03420471bd794c5ce53f
Niels Hoppe <mail@nielshoppe.de>
2024-04-22 13:47:38 +0200

    Add basic Python exercises

---
+++ src/basic_python_2.py
@@ -0,0 +1,31 @@
+import math
+
+def var(x: List[float]) -> float:
+    '''
+    Return the variance from a list of numbers.
+    '''
+    pass
+
+def std(x: List[float]) -> float:
+    '''
+    Return the standard deviation from a list of numbers.
+    '''
+    pass
+
+def sem(std: float, n: int) -> float:
+    '''
+    Return the standard error of the mean based on a given standard deviation and sample size.
+    '''
+    pass
+
+def mean(x: List[float]) -> float:
+    '''
+    Return the mean from a list of numbers.
+    '''
+    pass
+
+def median(x: List[float]) -> float:
+    '''
+    Return the median from a list of numbers.
+    '''
+    pass
